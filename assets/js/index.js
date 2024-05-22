d8b95b2f295ab79572496b4f1aeb2b71ab0f6446
Johnny Herrera <jmjaramillo95@gmail.com>
2024-05-22 03:44:12 -0500

    cv-presentar-portafolio-v25

--- assets/js/index.js
+++ assets/js/index.js
@@ -160,3 +160,20 @@ window.addEventListener('scroll', function () {
 })
 
 // flgs idioma
+
+//animaciones
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries)=> {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+}, {
+  threshold: 0.5,
+})
+
+hiddenElements.forEach(element => observer.observe(element))
